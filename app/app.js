fcbffa65048490cd626e185218d1a14951de0ec1
Daniel <peer77@gmail.com>
2018-06-08 10:05:35 +0300

    first commit - app test work

--- app/app.js
+++ app/app.js
@@ -1,14 +1,46 @@
-'use strict';
+﻿(function () {
+    'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    // Declare app level module which depends on views, and components
+    angular.module('app', ['ui.router'])
+        .controller('AppCtrl', AppCtrl)
+        .config(config);
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    AppCtrl.$inject = ['$scope']
+    function AppCtrl($scope) {
+    }
+
+    config.$inject = ['$urlRouterProvider', '$stateProvider']
+    function config($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('app/reviews');
+
+        $stateProvider
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: "../app/app.html",
+                controller: "AppCtrl"
+            })
+            .state('app.reviews', {
+                url: '/reviews',
+                component: 'reviews',
+                resolve: {
+                    model: function () {
+                        return {
+                            model: [
+                                { Id: 1, UserId: 1, Name: 'יוסי', Question: 'מה הולך?' },
+                                { Id: 2, UserId: 2, Name: 'משה', Question: 'מה העניינים?' },
+                                { Id: 3, UserId: 3, Name: 'רן', Question: 'מה שלום האישה?' },
+                                { Id: 4, UserId: 4, Name: 'אפריים', Question: 'מה שלום הילדים?' },
+                                { Id: 5, UserId: 5, Name: 'דודה', Question: 'מה הולך?' },
+                            ]
+                        }
+                    },
+                }
+
+            })
+
+    }
+
+   
+})();
