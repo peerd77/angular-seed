866d634e5f412f4b391e513f8edcc03fc81049d9
Daniel <peer77@gmail.com>
2018-06-08 10:07:32 +0300

    added untracked files

---
+++ app/components/reviews/reviews.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict'; 
+
+    angular.module('app')
+        .component('reviews', reviews());
+
+    function reviews() {
+        function ctrl() {
+            let $ctrl = this;
+            $ctrl.$onInit = function () {
+
+            }
+        }
+        return {
+            templateUrl: "./components/reviews/reviews.html",
+            controller: ctrl,
+            bindings: {
+                model: '<'
+            }
+        }
+    }
+})();
